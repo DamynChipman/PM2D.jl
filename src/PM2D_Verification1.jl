74053b83d86ade31504b313facce52405be43ae9
camperD <damyn.chipman@gmail.com>
2019-01-16 13:13:13 -0700

    Adding REQUIRE and updating for Julia 1.0 packaging

--- src/PM2D_Verification1.jl
+++ src/PM2D_Verification1.jl
@@ -42,3 +42,58 @@ end
 for i=1:NPAN-1
     CP_PM2D[i] = 1 - (norm(u_gamma([X[i],Y[i],0.0]))/(oper_cond[1]))^2
 end
+
+# == CP: PM2D w/ SimpleVPM ==
+
+# Load SimpleVPM
+using SimpleVPM
+vpm = SimpleVPM
+
+
+file_path = splitdir(@__FILE__)[1]      # Path to this file
+
+
+# Output options
+save_path = joinpath(file_path, "temps/test00/") # Path where to save paraview files
+run_name = "vortex"                     # Name of files
+paraview = true                         # Whether to call paraview
+prompt = true                           # Whether to prompt the user
+verbose = true                          # Whether to print verbose
+
+# ------------- SIMULATION PARAMETERS -------------------------------------
+sigma = 1.0               # Smoothing radius
+nu = 1e1                  # Kinematic viscosity
+
+magUinf = 0
+Uinf(t) = magUinf*[1, 0, 0]
+nsteps = 150              # Number of time steps
+dt = 0.001                # Time step
+
+nsteps_relax = 1          # Steps in between relaxation
+sgm0 = sigma              # Default core size
+beta_cs = 1.1            # Maximum core size growth
+
+# -------------- PARTICLE FIELD --------------------------------------------
+# Initiates the field
+pfield = ParticleField(nu,
+                        zeta_gauserf,       # Basis function
+                        g_gauserf;          # Regularizing function of zeta
+                        transposed=true,    # Transposed scheme
+                        relax=!true,        # Pedrizzeti relaxation scheme
+                        rlxf=0.3,           # Relaxation factor
+                        integration="euler",# Time integration scheme
+                        Uinf=Uinf
+                      )
+
+
+for i=1:NPAN-1
+    loc = [X[i], Y[i], 0.0]
+    Gamma = alpha_coefs[i]
+    vol = 1.0
+    particle = Particle(loc, Gamma, sigma, vol)
+    addparticle(pfield, particle)
+end
+
+for i=1:NPAN-1
+    CP_SVPM[i] = 1 - (norm(U(pfield, [X[i],Y[i],0.0]))/(oper_cond[1]))^2
+end
